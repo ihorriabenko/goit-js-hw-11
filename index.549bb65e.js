98e73a9fb1b48783de4a4ab995ef0c03f6c9149d
iryabenkoff <100082550+iryabenkoff@users.noreply.github.com>
2022-06-05 14:40:17 +0000

    Deploying to gh-pages from @ iryabenkoff/goit-js-hw-11@81533673a91724263f0de8eb37cdf21cc94418a6 🚀

--- index.549bb65e.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={form:document.querySelector(".header__form"),btnLoadMore:document.querySelector(".load-more")};e.form.addEventListener("submit",(function(e){e.preventDefault(),e.currentTarget.elements.text.value}));e.btnLoadMore.addEventListener("click",(function(e){console.log("more")}))}();
-//# sourceMappingURL=index.549bb65e.js.map
